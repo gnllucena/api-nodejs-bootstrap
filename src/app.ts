3c1a0b78f48859d227dd3cfdad393e9ac54d4c94
Gabriel Lucena <gnllucena@gmail.com>
2022-06-23 01:03:03 -0300

    Server up with routing-controllers

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,42 @@
-// import 'reflect-metadata';
+import 'reflect-metadata';
+import dotenv from 'dotenv';
+import { createExpressServer } from 'routing-controllers';
+import path from 'path';
 
-import { banner } from './lib/banner';
-import { Logger } from './lib/logger';
-import { expressLoader } from './configurations/expressLoader';
-import { iocLoader } from './configurations/iocLoader';
-import { monitorLoader } from './configurations/monitorLoader';
-import { swaggerLoader } from './configurations/swaggerLoader';
-import { typeormLoader } from './configurations/typeormLoader';
-import { winstonLoader } from './configurations/winstonLoader';
+dotenv.config();
 
-const log = new Logger(__filename);
+const port = process.env.PORT;
+const controllers = path.join(__dirname + '/controllers/*.js');
 
-bootstrapMicroframework({
-    loaders: [
-        winstonLoader,
-        iocLoader,
-        typeormLoader,
-        expressLoader,
-        swaggerLoader,
-        monitorLoader,
-    ],
-})
-    .then(() => banner(log))
-    .catch(error => log.error('Application is crashed: ' + error));
+createExpressServer({
+  controllers: [ controllers ],
+  cors: true,
+  classTransformer: true,
+  defaultErrorHandler: false,
+}).listen(port);
+
+// // import 'reflect-metadata';
+
+// import { banner } from './lib/banner';
+// import { Logger } from './lib/logger';
+// import { expressLoader } from './configurations/expressLoader';
+// import { iocLoader } from './configurations/iocLoader';
+// import { monitorLoader } from './configurations/monitorLoader';
+// import { swaggerLoader } from './configurations/swaggerLoader';
+// import { typeormLoader } from './configurations/typeormLoader';
+// import { winstonLoader } from './configurations/winstonLoader';
+
+// const log = new Logger(__filename);
+
+// bootstrapMicroframework({
+//     loaders: [
+//         winstonLoader,
+//         iocLoader,
+//         typeormLoader,
+//         expressLoader,
+//         swaggerLoader,
+//         monitorLoader,
+//     ],
+// })
+//     .then(() => banner(log))
+//     .catch(error => log.error('Application is crashed: ' + error));
